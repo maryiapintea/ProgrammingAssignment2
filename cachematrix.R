b2bcc76f8752ff557d7c2950cba5940f1e03e709
maryiapintea <maryiapintea@gmail.com>
2016-09-19 10:52:57 +0300

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,20 +1,32 @@
 ## The project does the invers of a given matrix
 ## Create matrix and Inverse the matrix
 
-## Write a short comment describing this function
+## Create a list to set the value of the vector, get the value of a vector
 
-makeCacheMatrix <- function(x = matrix(data, nrow, ncol, byrow = TRUE)) {
-  matrix(data, nrow, ncol, byrow = TRUE)
+makeCacheMatrix <- function(x=numeric()) {
+  set <- function(y) {
+  ## Sets the value of a vector
+  x <<- y
+   }
+  ## Gets the value of a vector
+  get <- function() x
+  list(set = set, get = get)
 }
 
 
 ## The inverse of a matrix
 
-cacheSolve <- function(x, ...) {
-  library(MASS)
-  inv <- ginv(x)
-  inv %*% x
-  zapsmall(inv %*% x)
+cacheSolve <- function(x, nr ...) {
+  ## Use the library for the inverse of function
+  library(MASS) 
+  ## Get the data
+  data <- x$get()
+  ## Create a matrix
+  matr<- matrix(data, nr, byrow)
+  ## Give the inverse of a matrix
+  inv <- ginv(matr)
+  inv %*% matr
+  zapsmall(inv %*% matr)
   ## Return a matrix that is the inverse of 'x'
 }
 
